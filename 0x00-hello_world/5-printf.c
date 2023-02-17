71cff4d3a6b15b592ec8f24f271b54e88a57a85f
Aquem25 <aquemlungz@gmail.com>
2023-02-17 00:26:52 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - A program that print a line using printf function
  * Return: 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		retun (0);
+		return (0);
 }
